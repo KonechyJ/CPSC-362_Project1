baff1f33c9845a3848df008ce5c588010064ec78
Unknown <Jkonechy@live.com>
2021-09-10 21:55:35 -0700

    Create the basic web page and a post
    Create the static web page to display info and a few dummy posts to test the pages ability to display info.

--- CPSC 362_Project1/Main.py
+++ CPSC 362_Project1/Main.py
@@ -1,5 +1,33 @@
-#comment
+from flask import Flask, render_template
+app = Flask(__name__)
 
-#Joshua Konechy
-#test
+#this a list of dictionaries currently being used to store "posts"
+posts = [
+    {
+        "author": "Joshua Konechy",
+        "title": "Post 1",
+        "content": "First Item",
+        "date_posted": "September 10, 2021"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Post 2",
+        "content": "Second Item",
+        "date_posted": "September 11, 2021"
+    }
+]
 
+#@app.route is a way to create a website directory
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts=posts)
+# render_template redirects the function to run the code in the respective .html files  listed
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
