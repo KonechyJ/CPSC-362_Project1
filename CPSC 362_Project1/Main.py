3a6bfad941d817cad1611ea9582f44cbf1f84dfd
Unknown <Jkonechy@live.com>
2021-09-10 22:24:58 -0700

    Added the necessary CSS and HTML to site
    Added some basic code snippets to the web site to create the basic layout. This template can be changed later to fit our needs.

--- CPSC 362_Project1/Main.py
+++ CPSC 362_Project1/Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 #this a list of dictionaries currently being used to store "posts"
