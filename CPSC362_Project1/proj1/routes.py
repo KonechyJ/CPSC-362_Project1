96b4a661215379d9403b9ce9fed7df8046a7aa2a
Andy <dongyicao123@csu.fullerton.edu>
2021-09-30 20:38:50 -0700

    Fixed the error for package structure

--- CPSC362_Project1/proj1/routes.py
+++ CPSC362_Project1/proj1/routes.py
@@ -2,9 +2,9 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm
-from flaskblog.models import User, Post
+from proj1 import app, db, bcrypt
+from proj1.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm
+from proj1.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
