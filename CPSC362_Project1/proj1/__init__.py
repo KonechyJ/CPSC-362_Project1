94550254556e99314208e0846dfe0bc56c870b60
Andy <dongyicao123@csu.fullerton.edu>
2021-10-17 19:54:09 -0700

    added custom error pages

--- CPSC362_Project1/proj1/__init__.py
+++ CPSC362_Project1/proj1/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from proj1.handlers import errors
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -19,5 +19,5 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
-
+app.register_blueprint(errors)
 from proj1 import routes
